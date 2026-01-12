a391ddfc9c0ce07773da8a6715187af48521f3cc
Tobias Nodland <tobias_olsen02@hotmail.com>
2026-01-12 11:16:54 +0100

    lab: øving 1 and 2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region Øvelse 1
 
-Console.WriteLine("Hello, World!");
+int value1 = 10;
+int value2 = 20;
+
+int addition = value1 + value2;
+int subtraction = value2 - value1;
+int multiplication = value1 * value2;
+double division = (double)value2 / value1;
+
+Console.WriteLine($"Resultat av addisjon: {addition}\nResultat av subtraksjon: {subtraction}\nResultat av multiplikasjon: {multiplication}\nResultat av divisjon: {division}\n");
+
+#endregion
+
+#region Øvelse 2
+
+int weekday = 7;
+
+if (weekday is >= 1 and <= 5)
+{
+    Console.WriteLine("Ukedag");
+}
+else if (weekday == 6 || weekday == 7)
+{
+    Console.WriteLine("Helg");
+}
+else
+{
+    Console.WriteLine("Ugyldig");
+}
+
+#endregion
