542312343085e82eaf865dd5fae33ce08adc2075
Tobias Nodland <tobias_olsen02@hotmail.com>
2026-01-19 16:00:00 +0100

    class: copy paste person class, clean namespace

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Numerics;
+using Lab3;
 
-Console.WriteLine("Hello, World!");
+bool task1Run = true;
+var persons = new Vector<Person>();
+
+while (task1Run)
+{
+    Console.WriteLine("Input person ID (or type 'exit' to quit): ");
+    string? input = Console.ReadLine();
+    if (input != null || input?.ToLower() != "exit")
+    {
+        
+    }
+    else
+    {
+        task1Run = false; 
+    }
+}
---
+++ Lab3/person.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab3;
+
+public class Person
+{
+    // Deklarer instansvariabler
+    private int id;
+    private string navn;
+    private DateOnly fødselsdato;
+    private string adresse;
+
+    // Konstruktøren
+    public Person()
+    {
+        this.id = 0;
+        this.navn = string.Empty;
+    }
+
+    // deklarer Set og Get metoder
+    public void SetId(int personId)
+    {
+        id = personId;
+    }
+
+    public void SetNavn(string personNavn)
+    {
+        navn = personNavn;
+    }
+
+    public int GetId()
+    {
+        return id;
+    }
+    public string GetNavn()
+    {
+        return navn;
+    }
+
+    // metoden som skal skrive ut info om person
+    public void VisInformasjon ()
+    {
+        Console.WriteLine("Person ID: " + id + " Person Name: " + navn);
+        // string interpolation
+        Console.WriteLine($"Person ID: {id} Person Name: {navn}");
+    }
+}
