5b9d622cc3c158ba4b7b40cd7d730d7a1ede046e
Tobias Nodland <tobias_olsen02@hotmail.com>
2026-01-19 17:23:50 +0100

    lab: øvelse 2

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,32 +1,75 @@
-﻿using System.Numerics;
-using Lab3;
+﻿using Lab3;
+#region Øvelse 1
+    var persons = new List<Person>(); //liste for å lagre person objekter
 
-var persons = new List<Person>();
-
-while (true)
-{
-    Console.WriteLine("Input person name (or type 'exit' to quit): ");
-    string? name = Console.ReadLine();
-    if (name == null || name.ToLower() == "exit")
+    while (true)
     {
-        break;
-    }
-    
-    Console.WriteLine("Input person ID (or type 'exit' to quit): ");
-    string? id = Console.ReadLine();
-    if (id == null || name?.ToLower() == "exit")
-    {
-        break;
-    }
-    int.TryParse(id, out int personId);
-
-    if (name != null)
-    {
+        Console.Write("Input person name (or type 'exit' to quit): ");
+        string? name = Console.ReadLine();
+        if (name == null || name.ToLower() == "exit")
+        {
+            break;
+        }
+        
+        Console.Write("Input person ID (or type 'exit' to quit): ");
+        string? id = Console.ReadLine();
+        if (id == null || name.ToLower() == "exit")
+        {
+            break;
+        }
+        int.TryParse(id, out int personId);
+        
         var person = new Person(personId, name);
         persons.Add(person);
     }
-    else
+#endregion
+#region Øvelse 2
+
+    int val1, val2;
+    char operation;
+
+    Console.Write("\nEnter first number: ");
+    int.TryParse(Console.ReadLine(), out val1);
+
+    Console.Write("Enter second number: ");
+    int.TryParse(Console.ReadLine(), out val2);
+
+    Console.Write("Enter operation (+, -, *, /): ");
+    char.TryParse(Console.ReadLine(), out operation);
+
+    switch (operation)
     {
-        Console.WriteLine("Invalid name input. Please try again.");
+        case '+':
+            Console.WriteLine($"{val1} + {val2} = {Mathops.Add(val1, val2)}");
+            break;
+        case '-':
+            Console.WriteLine($"{val1} - {val2} = {Mathops.Subtract(val1, val2)}");
+            break;
+        case '*':
+            Console.WriteLine($"{val1} * {val2} = {Mathops.Multiply(val1, val2)}");
+            break;
+        case '/':
+            if (val2 == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.\n");
+                break;
+            }
+            
+            Console.WriteLine($"{val1} / {val2} = {Mathops.Divide(val1, val2)}");
+            break;
+        default:
+            Console.WriteLine("Invalid operation.");
+            break;
     }
-}
+
+#endregion
+#region Øvelse 3
+
+
+
+#endregion
+#region Øvelse 4
+
+
+
+#endregion
---
+++ Lab3/mathops.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab3;
+
+public static class Mathops
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    
+    public static double Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Denominator cannot be zero.");
+        }
+        return (double)a / b;
+    }
+}
