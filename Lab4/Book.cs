fbdae6455ece4cb60ab5b5e86b8ef6f0e218aa73
Tobias Nodland <tobias_olsen02@hotmail.com>
2026-01-23 22:21:58 +0100

    lab4: complete

---
+++ Lab4/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4;
+
+public class Book(string title, string author, uint publicationYear)
+{
+    public string Title { get; set; } = title;
+    public string Author { get; set; } = author;
+    public uint PublicationYear { get; set; } = publicationYear;
+    
+    public string GetInfo()
+    {
+        return $"{Title} by {Author}, published in {PublicationYear}";
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,48 @@
+﻿using Lab4;
+
+bool task1 = true;
+var books = new List<Book>();
+while(task1)
+{
+    Console.WriteLine("Choices:\n[1] Add book\n[2] List books\n[3] Exit\n");
+    byte task1Choice = byte.Parse(Console.ReadLine() ?? string.Empty);
+    switch (task1Choice)
+    {
+        case 1:
+            Console.Clear();
+            
+            Console.WriteLine("Enter book title:");
+            string title = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter book author:");
+            string author = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter publication year:");
+            uint.TryParse(Console.ReadLine(), out uint publicationYear);
+            
+            books.Add(new Book(title, author, publicationYear));
+            
+            Console.Clear();
+            Console.WriteLine("Book added successfully!\nPress any key to continue...\n");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case 2:
+            Console.Clear();
+            Console.WriteLine("Books in collection:");
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine($"\t- {books[i].GetInfo()}");
+            }
+            Console.WriteLine("\nPress any key to continue...\n");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case 3:
+            Console.Clear();
+            task1 = false;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Invalid choice, please try again.\n");
+            break;
+    }
+}
