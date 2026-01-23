31ecaa9124e8fc5461454e6f7c15686308927e0c
Tobias Nodland <tobias_olsen02@hotmail.com>
2026-01-23 23:51:02 +0100

    lab4: task 2

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,44 +1,100 @@
 ﻿using Lab4;
 
-bool task1 = true;
-var books = new List<Book>();
-while(task1)
+#region Task 1
+    bool task1 = true;
+    var books = new List<Book>();
+    while(task1)
+    {
+        Console.WriteLine("Task 1 - Choices:\n[1] Add book\n[2] List books\n[3] Exit\n");
+        byte task1Choice = byte.Parse(Console.ReadLine() ?? string.Empty);
+        
+        switch (task1Choice)
+        {
+            case 1:
+                Console.Clear();
+                
+                Console.WriteLine("Enter book title:");
+                string title = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Enter book author:");
+                string author = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Enter publication year:");
+                uint.TryParse(Console.ReadLine(), out uint publicationYear);
+                
+                books.Add(new Book(title, author, publicationYear));
+                
+                Console.Clear();
+                Console.WriteLine("Book added successfully!\nPress any key to continue...\n");
+                Console.ReadKey();
+                Console.Clear();
+                break;
+            case 2:
+                Console.Clear();
+                Console.WriteLine("Books in collection:");
+                for (int i = 0; i < books.Count; i++)
+                {
+                    Console.WriteLine($"\t- {books[i].GetInfo()}");
+                }
+                Console.WriteLine("\nPress any key to continue...\n");
+                Console.ReadKey();
+                Console.Clear();
+                break;
+            case 3:
+                Console.Clear();
+                task1 = false;
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("Invalid choice, please try again.\n");
+                break;
+        }
+    }
+#endregion
+
+#region Task 2
+
+bool task2 = true; 
+var users = new List<UserAccount>();
+    
+while (task2)
 {
-    Console.WriteLine("Choices:\n[1] Add book\n[2] List books\n[3] Exit\n");
-    byte task1Choice = byte.Parse(Console.ReadLine() ?? string.Empty);
-    switch (task1Choice)
+    Console.WriteLine("Task 2 - Choices:\n[1] Create account\n[2] List users\n[3] Select user\n[4] Exit\n");
+    byte task2Choice = byte.Parse(Console.ReadLine() ?? string.Empty);
+
+    switch (task2Choice)
     {
         case 1:
             Console.Clear();
-            
-            Console.WriteLine("Enter book title:");
-            string title = Console.ReadLine() ?? string.Empty;
-            Console.WriteLine("Enter book author:");
-            string author = Console.ReadLine() ?? string.Empty;
-            Console.WriteLine("Enter publication year:");
-            uint.TryParse(Console.ReadLine(), out uint publicationYear);
-            
-            books.Add(new Book(title, author, publicationYear));
-            
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine() ?? string.Empty;
+
+            users.Add(new UserAccount(username, password));
+
             Console.Clear();
-            Console.WriteLine("Book added successfully!\nPress any key to continue...\n");
+            Console.WriteLine("Account created successfully!\nPress any key to continue...\n");
             Console.ReadKey();
             Console.Clear();
             break;
         case 2:
             Console.Clear();
-            Console.WriteLine("Books in collection:");
-            for (int i = 0; i < books.Count; i++)
+            Console.WriteLine($"Total users: {users.Count}");
+            Console.WriteLine($"| {"Username",-50}| {"Created at",-50}|");
+            foreach (var user in users)
             {
-                Console.WriteLine($"\t- {books[i].GetInfo()}");
+                Console.WriteLine($"| {user.Username,-50}| {user.CreatedAt,-50}|");  
             }
+            
             Console.WriteLine("\nPress any key to continue...\n");
             Console.ReadKey();
             Console.Clear();
             break;
         case 3:
+            UserMenu();
+            break;
+        case 4:
             Console.Clear();
-            task1 = false;
+            task2 = false;
             break;
         default:
             Console.Clear();
@@ -46,3 +102,70 @@ while(task1)
             break;
     }
 }
+    
+void UserMenu()
+{
+    Console.Clear();
+    Console.WriteLine("Enter username of the account to select:");
+    string selectedUsername = Console.ReadLine() ?? string.Empty;
+    var selectedUser = users.Find(u => u.Username == selectedUsername);
+    
+    if (selectedUser == null)
+    {
+        Console.Clear();
+        Console.WriteLine("User not found.\nPress any key to continue...\n");
+        Console.ReadKey();
+        Console.Clear();
+        return;
+    }
+    
+    bool userMenuActive = true;    
+    while (userMenuActive)
+    {
+        Console.Clear();
+        Console.WriteLine($"User {selectedUser.Username} selected. Select an option:\n[1] Display account info\n[2] Change password\n[3] Back to main menu\n");
+        byte userMenuChoice = byte.Parse(Console.ReadLine() ?? string.Empty);
+
+        switch (userMenuChoice)
+        {
+            case 1:
+                Console.Clear();
+                Console.WriteLine("Enter password to display account info:");
+                string infoPassword = Console.ReadLine() ?? string.Empty;
+                selectedUser.DisplayAccountInfo(infoPassword);
+            
+                Console.WriteLine("\nPress any key to continue...\n");
+                Console.ReadKey();
+                Console.Clear();
+                break;
+            case 2:
+                Console.Clear();
+                Console.WriteLine("Enter current password:");
+                string currentPassword = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Enter new password:");
+            
+                string newPassword = Console.ReadLine() ?? string.Empty;
+            
+                Console.Clear();
+                selectedUser.ChangePassword(currentPassword, newPassword);
+            
+                Console.WriteLine("\nPress any key to continue...\n");
+                Console.ReadKey();
+                Console.Clear();
+                break;
+            case 3:
+                Console.Clear();
+                userMenuActive = false;
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("Invalid choice, please try again.\n");
+                Console.WriteLine("Press any key to continue...\n");
+                Console.ReadKey();
+                Console.Clear();
+                break;
+        }
+    }
+}
+
+#endregion
---
+++ Lab4/UserAccount.cs
@@ -0,0 +1,48 @@
+﻿namespace Lab4;
+
+public class UserAccount
+{
+    private List<string> _usedPasswords = new List<string>();
+    private string _password;
+    public string Username { get; private set; } 
+    public DateTime CreatedAt { get; init; }
+    
+    public UserAccount(string username, string password)
+    {
+        Username = username;
+        _password = password;
+        CreatedAt = DateTime.Now;
+        
+        _usedPasswords.Add(password);
+    }
+    
+    public void ChangePassword(string oldPassword, string newPassword)
+    {
+        if (_usedPasswords.Contains(newPassword))
+        {
+            Console.WriteLine("This password has been used before. Please choose a different password.");
+            return;
+        }
+        if (oldPassword != _password)
+        {
+            Console.WriteLine("Enter current password to change password:");
+            return;
+        }
+        
+        _password = newPassword;
+        _usedPasswords.Add(newPassword);
+        Console.WriteLine("Password changed successfully.");
+    }
+    
+   public void DisplayAccountInfo(string password)
+   {
+       if (password != _password)
+       {
+           Console.WriteLine("Incorrect password.");
+           return;
+       }
+       
+       Console.WriteLine($"Username: {Username}\nAccount Created At: {CreatedAt}\nCompleted Password Changes: {_usedPasswords.Count - 1}" +
+                         $"\nCurrent Password: {_password}");
+   }
+}
