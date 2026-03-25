e775d82561ddb34345e43c774805eb43a3df61b8
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-03-25 12:36:05 +0100

    lab 10 projects

---
+++ Lab10Calc.Tests/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Xunit;
+
+namespace Lab10Calc.Tests;
+
+[TestSubject(typeof(Calculator))]
+public class CalculatorTest
+{
+    [Fact]
+    public void TestAddNumbers()
+    {
+        //Arrange
+        double result;
+
+        //Act
+        result = Calculator.AddNumbers(3, 5);
+
+        //Assert
+        Assert.Equal(8, result);
+    }
+}
---
+++ Lab10Calc/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab10Calc;
+
+public static class Calculator
+{
+    public static double AddNumbers(int tall1, int tall2)
+    {
+        return tall1 + tall2;
+    }
+
+    public static double SubtractNumbers(int tall1, int tall2)
+    {
+        return tall1 - tall2;
+    }
+
+    public static double MultiplyNumbers(int tall1, int tall2)
+    {
+        return tall1 * tall2;
+    }
+
+    public static double DivideNumbers(int tall1, int tall2)
+    {
+        return tall1 / (double)tall2;
+    }
+}
