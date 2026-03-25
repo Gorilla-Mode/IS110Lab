376d751255dc5ae5a60f03955607fe65846271c6
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-03-25 13:55:46 +0100

    lab10: input handling and tests

--- Lab10Calc.Tests/CalculatorTest.cs
+++ Lab10Calc.Tests/CalculatorTest.cs
@@ -10,12 +10,61 @@ public class CalculatorTest
     public void TestAddNumbers()
     {
         //Arrange
+        double expectedResult = 8;
         double result;
 
         //Act
         result = Calculator.AddNumbers(3, 5);
 
         //Assert
-        Assert.Equal(8, result);
+        Assert.Equal(expectedResult, result);
     }
+
+    [Fact]
+    public void TestSubtractNumbers()
+    {
+        //Arrange
+        double expectedResult = 2;
+        double result;
+        
+        //Act
+        result = Calculator.SubtractNumbers(5, 3);
+        
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void TestMultiplyNumbers()
+    {
+        //Arrange
+        double expectedResult = 10;
+        double expectedResultMultByZero = 0;
+        double expectedResultMultByNegative = -10;
+
+        //act
+        var result = Calculator.MultiplyNumbers(2, 5);
+        var resultMultByZero = Calculator.MultiplyNumbers(2, 0);
+        var resultMultByNegative = Calculator.MultiplyNumbers(2, -5);
+           
+
+        //assert
+        Assert.Equal(expectedResult, result);
+        Assert.Equal(expectedResultMultByZero, resultMultByZero);
+        Assert.Equal(expectedResultMultByNegative, resultMultByNegative);
+    }
+
+    [Fact]
+    public void TestDivideNumbers()
+    {
+        //Arrange
+        double expectedResult = 2;
+        
+        //act
+        var result = Calculator.DivideNumbers(4, 2);
+        
+        //assert
+        Assert.Equal(expectedResult, result);
+    }
+    
 }
--- Lab10Calc/Program.cs
+++ Lab10Calc/Program.cs
@@ -1,3 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Lab10Calc;
 
-Console.WriteLine("Hello, World!");
+enum MenuOptions
+{
+    Exit = 0,
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        for (bool run = true; run;)
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n0. Exit\n");
+
+            if (!Enum.TryParse(Console.ReadLine(), out MenuOptions result))
+            {
+                InvalidOption();
+                continue;
+            }
+            
+            Action option = result switch
+            {
+                MenuOptions.Add      => AddNumbers,
+                MenuOptions.Subtract => SubtractNumbers,
+                MenuOptions.Multiply => MultiplyNumbers,
+                MenuOptions.Divide   => DivideNumbers,
+                MenuOptions.Exit     => () => run = false,
+                _                    => InvalidOption
+            };
+            
+            option();
+        }
+
+        int[] getInput()
+        {
+            Console.Write("Input first number : ");
+            if (!int.TryParse(Console.ReadLine(), out int tall1))
+            {
+                Console.WriteLine("Invalid input");
+                getInput();
+            }
+
+            Console.Write("\nInput second number: ");
+            if (!int.TryParse(Console.ReadLine(), out int tall2))
+            {
+                Console.WriteLine("Invalid input");
+                getInput();
+            }
+            
+            return [tall1, tall2];
+        }
+        
+        void AddNumbers()
+        {
+            Console.WriteLine("Add numbers");
+            
+            var numbers= getInput();
+            Console.WriteLine($"Result: {Calculator.AddNumbers(numbers[0], numbers[1])}");
+        }
+    
+        void SubtractNumbers()
+        {
+            Console.WriteLine("Subtract numbers");
+            
+            var numbers = getInput();
+            Console.WriteLine($"Result: {Calculator.SubtractNumbers(numbers[0], numbers[1])}");
+        }
+    
+        void MultiplyNumbers()
+        {
+            Console.WriteLine("Multiply numbers");
+            
+            var numbers = getInput();
+            Console.WriteLine($"Result: {Calculator.MultiplyNumbers(numbers[0], numbers[1])}");
+        }
+    
+        void DivideNumbers()
+        {
+            Console.WriteLine("Divide numbers");
+            
+            var numbers = getInput();
+            Console.WriteLine($"Result: {Calculator.DivideNumbers(numbers[0], numbers[1])}");
+        }
+        
+        void InvalidOption()
+        {
+            Console.Clear();
+            Console.WriteLine("Invalid option\nPress any key to continue..");
+            Console.Read();
+        }
+    }
+}
